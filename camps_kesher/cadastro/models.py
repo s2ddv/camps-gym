7e69c1800e80101c79f51d9a2a7c212eb32be7ee
elocmachado <eloisamachado.dev2024@gmail.com>
2025-10-02 14:31:40 -0300

    database

--- camps_kesher/cadastro/models.py
+++ camps_kesher/cadastro/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class Usuario(AbstractUser):
     idade = models.PositiveIntegerField(null=True, blank=True)
     telefone = models.CharField(max_length=20, null=True, blank=True)
+    
      
     def __str__(self):
         return self.username
