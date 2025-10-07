ad1e85ccb1bc7e312e93c3487ffd12a258675bd3
Clxt0Tm <guilhermebarbi.dev2024@gmail.com>
2025-10-07 15:37:58 -0300

    main-css

--- frontend/src/lib/features/menu.js
+++ frontend/src/lib/features/menu.js
@@ -6,3 +6,10 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+
+document.addEventListener('click', (event) => {
+  if (!hamMenu.contains(event.target) && !offScreenMenu.contains(event.target)) {
+    hamMenu.classList.remove('active');
+    offScreenMenu.classList.remove('active');
+  }
+});
