2ebe7051a14acdad2c9341397d72dcb289d19d80
elocmachado <eloisamachado.dev2024@gmail.com>
2025-10-14 08:21:07 -0300

    Banco de Dados Completo: Django

--- camps_kesher/produtos/models.py
+++ camps_kesher/produtos/models.py
@@ -21,7 +21,7 @@ class Produto(models.Model):
     cor = models.CharField(max_length=50)
     tamanho = models.CharField(max_length=20, choices = Tamanho.TAMANHO)
     preco = models.DecimalField(max_digits=8, decimal_places=2)
-
+    imagem = models.ImageField(upload_to="produtos/", null=True, blank=True)
     def __str__(self):
         return self.nome
 
--- camps_kesher/usuarios/serializers.py
+++ camps_kesher/usuarios/serializers.py
@@ -5,29 +5,30 @@ class FisicoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fisico
         fields = "__all__"
+
 class UsuarioSerializer(serializers.ModelSerializer):
-    físico = FisicoSerializer(read_only=True)
+    fisico = FisicoSerializer(read_only=True)
 
     class Meta:
         model = Usuario
-        fields = ["id", "nome_de_usuário","primeiro_nome", "último_nome", "email", "data_de_nascimento", "telefone", "fisico"]
+        fields = ["id", "username", "first_name", "last_name", "email", "data_de_nascimento", "telefone", "fisico"]
 
-        class UsuarioCreateSerializer(serializers.ModelSerializer):
-            password = serializers.CharField(write_only=True)
+class UsuarioCreateSerializer(serializers.ModelSerializer):
+    senha = serializers.CharField(write_only=True)
 
-            class Meta:
-                model = Usuario
-                fields = ["id", "nome_de_usuário","primeiro_nome", "último_nome", "email", "data_de_nascimento", "telefone", "senha"]
+    class Meta:
+        model = Usuario
+        fields = ["id", "username", "first_name", "last_name", "email", "data_de_nascimento", "telefone", "senha"]
 
-            def create(self, validated_data):
-                user = Usuario(
-                    nome_de_usuario=validated_data["nome_de_usuário"],
-                    primeiro_nome=validated_data["primeiro_nome"],
-                    ultimo_nome=validated_data["último_nome"],
-                    email=validated_data["email"],
-                    data_de_nascimento=validated_data.get("data_de_nascimento"),
-                    telefone=validated_data.get("telefone"),
-                )
-                user.set.password(validated_data["senha"])
-                user.save()
-                return user
+    def create(self, validated_data):
+        user = Usuario(
+            username=validated_data["username"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            email=validated_data["email"],
+            data_de_nascimento=validated_data.get("data_de_nascimento"),
+            telefone=validated_data.get("telefone"),
+        )
+        user.set_password(validated_data["senha"])
+        user.save()
+        return user
